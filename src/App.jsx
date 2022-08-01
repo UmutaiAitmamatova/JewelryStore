fd28838fa90c197f2bdd5812b221f3d131c13e5f
Umutai <umutaiaitmamativa@gmail.com>
2022-08-01 22:13:23 +0300

    finished Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Header, Footer } from './components'
+import { Header, Footer } from './components';
+import { Home, Cart } from './pages'
 import { BrowserRouter as Router, Routes, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className='wrapper'>
-      <Header/>
+    <Router>
+      <div className='wrapper'>
+        <Header />
 
-      <div className="content">
-        <h1>Hello world!</h1>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/app/:title" element={<Cart />} />
+          </Routes>
+
+        </div>
+
+        <Footer />
       </div>
-
-      <Footer/>
-    </div>
+    </Router>
   );
 }
 
---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+export {default as Rings} from '../assets/rings.png';
+export { default as Errings } from '../assets/earrings.png';
+export { default as Bracelet } from '../assets/bracelet.png';
+export { default as Watch } from '../assets/watch.png';
