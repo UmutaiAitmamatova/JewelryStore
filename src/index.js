46c0abfa0b8a0f30b20a0bf5f818dc428ef5ba27
Umutai <umutaiaitmamativa@gmail.com>
2022-08-09 00:40:13 +0300

    added redux

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.scss';
-
 import { BrowserRouter as Router } from "react-router-dom";
+import store from './redux/store'
+import { Provider } from 'react-redux'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+console.log(store);
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>
 );
